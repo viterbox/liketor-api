fafd13ca778ba34a188f72894cb16c64933bda53
franco viterbo <francoviterbo@gmail.com>
2017-07-07 18:44:20 -0500

    routes added

---
+++ config/routes.js
@@ -0,0 +1,13 @@
+"use strict";
+
+
+exports.endoints = [
+    {
+        method:"GET", 
+        path:"/movies/now_playing", 
+        handler: function (request, reply) {
+            reply("Hello");
+        }
+    }
+];
+
