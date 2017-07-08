c8ca923ce5efb1d1f60efad34e34d5052eecb4b0
franco viterbo <francoviterbo@gmail.com>
2017-07-08 12:39:33 -0500

    Datasources added on server

--- server.js
+++ server.js
@@ -2,16 +2,27 @@
 
 const Hapi = require("hapi");
 const routes = require("./config/routes");
+const dataSource = require("./config/dataSource");
 
 const server = new Hapi.Server();
 server.connection({ port: 8000, host: "localhost" });
 
 server.route(routes.endoints);
 
-server.start((err) => {
-
+dataSource.connect((err)=> {
     if (err) {
         throw err;
     }
-    console.log(`Server running at: ${server.info.uri}`);
+
+    console.log("DataSourcer connected");
+
+    server.start((err) => {
+
+        if (err) {
+            throw err;
+        }
+        console.log(`Server running at: ${server.info.uri}`);
+    });
+
 });
+
